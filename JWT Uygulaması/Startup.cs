572ef445e6aea3e8ed3a3a56efc24b73b09ec590
= <=>
2021-03-19 23:59:57 +0300

    Jwt paketi uygulamaya dahil edildi ve Startup cs de tanımlamalar yapıldı

--- JWT Uygulaması/Startup.cs
+++ JWT Uygulaması/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,9 +6,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace JWT_Uygulaması
@@ -21,13 +24,23 @@ namespace JWT_Uygulaması
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+       
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt=> { opt.RequireHttpsMetadata = false;
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = "http://localhost", //Tokeni oluşturan
+                    ValidAudience = "http://localhost",//Tokeni kim kullanacak?
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("gulergulerguler1")),//Key değeri
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime=true
+                };
+            });  
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
